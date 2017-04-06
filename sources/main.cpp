3ac683994315a39cd268a50e44720a7718ed5fb8
Michael Lee <mikecovlee@163.com>
2017-04-06 10:51:45 +0800

    支持array类型

--- sources/main.cpp
+++ sources/main.cpp
@@ -403,6 +403,7 @@ namespace cov_basic {
 		storage.add_type("number",[]()->cov::any {return number(0);});
 		storage.add_type("boolean",[]()->cov::any {return boolean(true);});
 		storage.add_type("string",[]()->cov::any {return string();});
+		storage.add_type("array",[]()->cov::any {return array();});
 		add_function(input);
 		add_function(getline);
 		add_function(print);
