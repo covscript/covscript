52c2dc74e26ce59fb1874de6ebe24064a1fa21c8
Michael Lee <mikecovlee@163.com>
2017-04-17 17:47:30 +0800

    修复部分错误

--- sources/covbasic.hpp
+++ sources/covbasic.hpp
@@ -338,6 +338,11 @@ namespace cov_basic {
 namespace cov_basic {
 	cov::any _clone(cov::any val)
 	{
+		if(val.type()=typeid(linker)) {
+			cov::any v=val.const_val<linker>().data;
+			v.clone();
+			return v;
+		}
 		val.clone();
 		if(val.type()==typeid(array))
 			for(cov::any& v:val.val<array>(true))
