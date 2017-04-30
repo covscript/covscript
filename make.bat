7c6785fa85095d01073a27e27a33cff6b87a518f
Michael Lee <mikecovlee@163.com>
2017-04-30 15:13:43 +0800

    更新编译脚本

--- make.bat
+++ make.bat
@@ -1,1 +1,3 @@
-g++ -std=c++11 -static -fPIE -s -O3 .\sources\main.cpp -o cbs2.1.exe
+@windres .\cbs2.1.rc cbs2.1.o
+@g++ -std=c++11 -static -fPIE -s -O3 .\sources\main.cpp cbs2.1.o -o cbs2.1.exe
+@del /F /Q cbs2.1.o
