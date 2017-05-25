865d345ac349236c48d0e03a3933ca0501f2b0fb
Michael Lee <mikecovlee@163.com>
2017-05-25 13:44:18 +0800

    修复Darwin Input Box在Windows上的错误

--- format.bat
+++ format.bat
@@ -1,9 +1,9 @@
 @del /F /Q .\sources\*.gch
 @astyle .\sources\*.*
-@astyle .\sources\*\*.*
+@astyle .\sources\extensions\*.*
 @del /F /Q .\sources\*.orig
-@del /F /Q .\sources\*\*.orig
+@del /F /Q .\sources\extensions\*.orig
 @astyle -A4 -N -t .\sources\*.*
-@astyle -A4 -N -t .\sources\*\*.*
+@astyle -A4 -N -t .\sources\extensions\*.*
 @del /F /Q .\sources\*.orig
-@del /F /Q .\sources\*\*.orig
+@del /F /Q .\sources\extensions\*.orig
