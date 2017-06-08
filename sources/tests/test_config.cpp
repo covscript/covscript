256d2e50c7214bbbab1e24783be64816c9f7e722
Michael Lee <mikecovlee@163.com>
2017-06-08 22:27:43 +0800

    增加测试
    测试CBS作为C++程序的配置文件

---
+++ sources/tests/test_config.cpp
@@ -0,0 +1,17 @@
+#define CBS_STATIC
+#include "../covbasic.hpp"
+#include <iostream>
+int main()
+{
+	using namespace cov_basic;
+	cov_basic::reset();
+	cov_basic::cov_basic("./config.cbs");
+	const hash_map& config=runtime->storage.get_var("test_configure").const_val<hash_map>();
+	std::cout<<"File Path:"<<config.at("path")<<std::endl;
+	std::cout<<"File Info:"<<std::endl;;
+	std::cout<<"Width:"<<config.at("width")<<std::endl;
+	std::cout<<"Height:"<<config.at("height")<<std::endl;
+	std::cout<<"Size:"<<config.at("width").const_val<number>()*config.at("height").const_val<number>()<<std::endl;
+	std::cout<<"Format:"<<config.at("format")<<std::endl;
+	return 0;
+}
