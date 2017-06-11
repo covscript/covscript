78b74ee24d0b0de5f1a0d9011d15e8f25cb72806
Michael Lee <mikecovlee@163.com>
2017-06-11 13:05:51 +0800

    支持定义编译期常量

--- sources/symbols.hpp
+++ sources/symbols.hpp
@@ -6,7 +6,7 @@ namespace cov_basic {
 		null,endline,action,signal,id,value,sblist,mblist,lblist,expr,arglist,array
 	};
 	enum class action_types {
-		import_,struct_,block_,endblock_,define_,as_,if_,then_,else_,switch_,case_,default_,while_,loop_,until_,do_,for_,to_,step_,break_,continue_,function_,return_
+		import_,struct_,block_,endblock_,define_,constant_,as_,if_,then_,else_,switch_,case_,default_,while_,loop_,until_,do_,for_,to_,step_,break_,continue_,function_,return_
 	};
 	enum class signal_types {
 		add_,sub_,mul_,div_,mod_,pow_,com_,dot_,und_,abo_,asi_,equ_,ueq_,aeq_,neq_,and_,or_,not_,inc_,dec_,pair_,slb_,srb_,mlb_,mrb_,llb_,lrb_,esb_,emb_,elb_,link_,mem_,fcall_,access_,new_,typeid_
@@ -166,7 +166,7 @@ namespace cov_basic {
 		{"()",signal_types::esb_},{"[]",signal_types::emb_},{"{}",signal_types::elb_},{"++",signal_types::inc_},{"--",signal_types::dec_},{"&",signal_types::link_},{"->",signal_types::mem_},{":",signal_types::pair_}
 	};
 	mapping<std::string,action_types> action_map= {
-		{"import",action_types::import_},{"struct",action_types::struct_},{"block",action_types::block_},{"end",action_types::endblock_},{"define",action_types::define_},{"as",action_types::as_},{"if",action_types::if_},/*{"then",action_types::then_},*/{"else",action_types::else_},{"switch",action_types::switch_},{"case",action_types::case_},{"default",action_types::default_},
+		{"import",action_types::import_},{"struct",action_types::struct_},{"block",action_types::block_},{"end",action_types::endblock_},{"define",action_types::define_},{"constant",action_types::constant_},{"as",action_types::as_},{"if",action_types::if_},/*{"then",action_types::then_},*/{"else",action_types::else_},{"switch",action_types::switch_},{"case",action_types::case_},{"default",action_types::default_},
 		{"while",action_types::while_},{"until",action_types::until_},{"loop",action_types::loop_},/*{"do",action_types::do_},*/{"for",action_types::for_},{"to",action_types::to_},{"step",action_types::step_},{"break",action_types::break_},{"continue",action_types::continue_},{"function",action_types::function_},{"return",action_types::return_}
 	};
 	enum class constant_values {
