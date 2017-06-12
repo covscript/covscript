b3780e3d88a0303e65b906e82dbb89914870c8d8
Michael Lee <mikecovlee@163.com>
2017-06-12 12:52:20 +0800

    完善Api

--- sources/extensions/system_extension.hpp
+++ sources/extensions/system_extension.hpp
@@ -2,6 +2,7 @@
 #include "../arglist.hpp"
 #include <iostream>
 #include <cstdlib>
+#include <limits>
 static cov_basic::extension system_ext;
 namespace system_cbs_ext {
 	using namespace cov_basic;
@@ -57,8 +58,16 @@ namespace system_cbs_ext {
 			throw lang_error("Environment variable \""+args.at(0).const_val<string>()+"\" is not exist.");
 		return string(str);
 	}
+	cov::any exit(array& args)
+	{
+		arglist::check<number>(args);
+		std::exit(args.at(0).const_val<number>());
+		return number(0);
+	}
 	void init()
 	{
+		system_ext.add_var("max",std::numeric_limits<number>::max());
+		system_ext.add_var("inf",std::numeric_limits<number>::infinity());
 		system_ext.add_var("input",cov::any::make_constant<native_interface>(input));
 		system_ext.add_var("getline",cov::any::make_constant<native_interface>(getline));
 		system_ext.add_var("print",cov::any::make_constant<native_interface>(print));
@@ -66,5 +75,6 @@ namespace system_cbs_ext {
 		system_ext.add_var("setprecision",cov::any::make_constant<native_interface>(setprecision));
 		system_ext.add_var("run",cov::any::make_constant<native_interface>(run));
 		system_ext.add_var("getenv",cov::any::make_constant<native_interface>(getenv));
+		runtime_ext.add_var("exit",cov::any::make_constant<native_interface>(exit));
 	}
 }
