4a9895e30b070a490fba5ca667bd10f697ec0019
Michael Lee <mikecovlee@163.com>
2017-06-20 08:16:10 +0800

    支持报错时详细至文件

--- sources/exceptions.hpp
+++ sources/exceptions.hpp
@@ -10,6 +10,8 @@ namespace cov_basic {
 			mWhat("\nCovariant Basic Syntax Error:\n"+str) {}
 		syntax_error(std::size_t line,const std::string& str) noexcept:
 			mWhat("\nIn line "+std::to_string(line)+":"+str) {}
+		syntax_error(const std::string& file,std::size_t line,const std::string& str) noexcept:
+			mWhat("\nIn file \""+file+"\",line "+std::to_string(line)+":"+str) {}
 		syntax_error(const syntax_error&)=default;
 		syntax_error(syntax_error&&)=default;
 		virtual ~syntax_error()=default;
@@ -28,6 +30,8 @@ namespace cov_basic {
 			mWhat("\nCovariant Basic Internal Error:\n"+str) {}
 		internal_error(std::size_t line,const std::string& str) noexcept:
 			mWhat("\nIn line "+std::to_string(line)+":"+str) {}
+		internal_error(const std::string& file,std::size_t line,const std::string& str) noexcept:
+			mWhat("\nIn file \""+file+"\",line "+std::to_string(line)+":"+str) {}
 		internal_error(const internal_error&)=default;
 		internal_error(internal_error&&)=default;
 		virtual ~internal_error()=default;
@@ -46,6 +50,8 @@ namespace cov_basic {
 			mWhat("\nCovariant Basic Language Error:\n"+str) {}
 		lang_error(std::size_t line,const std::string& str) noexcept:
 			mWhat("\nIn line "+std::to_string(line)+":"+str) {}
+		lang_error(const std::string& file,std::size_t line,const std::string& str) noexcept:
+			mWhat("\nIn file \""+file+"\",line "+std::to_string(line)+":"+str) {}
 		lang_error(const lang_error&)=default;
 		lang_error(lang_error&&)=default;
 		virtual ~lang_error()=default;
