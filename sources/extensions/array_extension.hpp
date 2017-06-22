f438b09534661e194c08f8a51db2912294db3d46
Michael Lee <mikecovlee@163.com>
2017-06-22 08:54:20 +0800

    将array扩展改为CNI

--- sources/extensions/array_extension.hpp
+++ sources/extensions/array_extension.hpp
@@ -3,62 +3,42 @@
 static cov_basic::extension array_ext;
 namespace array_cbs_ext {
 	using namespace cov_basic;
-	cov::any push_front(array& args)
+	void push_front(array& arr,const cov::any& val)
 	{
-		if(args.size()!=2)
-			throw syntax_error("Wrong size of arguments.");
-		if(args.at(0).type()!=typeid(array))
-			throw syntax_error("Wrong type of arguments.(Request Array)");
-		args.at(0).val<array>(true).push_front(copy(args.at(1)));
-		return number(0);
+		arr.push_front(val);
 	}
-	cov::any pop_front(array& args)
+	void pop_front(array& arr)
 	{
-		if(args.size()!=1)
-			throw syntax_error("Wrong size of arguments.");
-		if(args.at(0).type()!=typeid(array))
-			throw syntax_error("Wrong type of arguments.(Request Array)");
-		args.at(0).val<array>(true).pop_front();
-		return number(0);
+		arr.pop_front();
 	}
-	cov::any push_back(array& args)
+	void push_back(array& arr,const cov::any& val)
 	{
-		if(args.size()!=2)
-			throw syntax_error("Wrong size of arguments.");
-		if(args.at(0).type()!=typeid(array))
-			throw syntax_error("Wrong type of arguments.(Request Array)");
-		args.at(0).val<array>(true).push_back(copy(args.at(1)));
-		return number(0);
+		arr.push_back(val);
 	}
-	cov::any pop_back(array& args)
+	void pop_back(array& arr)
 	{
-		if(args.size()!=1)
-			throw syntax_error("Wrong size of arguments.");
-		if(args.at(0).type()!=typeid(array))
-			throw syntax_error("Wrong type of arguments.(Request Array)");
-		args.at(0).val<array>(true).pop_back();
-		return number(0);
+		arr.pop_back();
 	}
-	cov::any clear(array& args)
+	void clear(array& arr)
 	{
-		if(args.size()!=1)
-			throw syntax_error("Wrong size of arguments.");
-		if(args.at(0).type()!=typeid(array))
-			throw syntax_error("Wrong type of arguments.(Request Array)");
-		args.at(0).val<array>(true).clear();
-		return number(0);
+		arr.clear();
 	}
 	cov::any at(const array& arr,number posit)
 	{
 		return arr.at(posit);
 	}
+	number size(const array& arr)
+	{
+		return arr.size();
+	}
 	void init()
 	{
-		array_ext.add_var("push_front",cov::any::make_protect<native_interface>(push_front,true));
-		array_ext.add_var("pop_front",cov::any::make_protect<native_interface>(pop_front,true));
-		array_ext.add_var("push_back",cov::any::make_protect<native_interface>(push_back,true));
-		array_ext.add_var("pop_back",cov::any::make_protect<native_interface>(pop_back,true));
-		array_ext.add_var("clear",cov::any::make_protect<native_interface>(clear,true));
+		array_ext.add_var("push_front",cov::any::make_protect<native_interface>(cni(push_front),true));
+		array_ext.add_var("pop_front",cov::any::make_protect<native_interface>(cni(pop_front),true));
+		array_ext.add_var("push_back",cov::any::make_protect<native_interface>(cni(push_back),true));
+		array_ext.add_var("pop_back",cov::any::make_protect<native_interface>(cni(pop_back),true));
+		array_ext.add_var("clear",cov::any::make_protect<native_interface>(cni(clear),true));
 		array_ext.add_var("at",cov::any::make_protect<native_interface>(cni(at),true));
+		array_ext.add_var("size",cov::any::make_protect<native_interface>(cni(size),true));
 	}
 }
