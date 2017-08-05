0a588121fc6a9b1a07910f0a56b407787113db91
Michael Lee <mikecovlee@163.com>
2017-08-05 22:58:38 +0800

    为Array增加迭代器的支持

--- sources/extensions/array_extension.hpp
+++ sources/extensions/array_extension.hpp
@@ -8,8 +8,72 @@ namespace cs_any {
 		return array_ext_shared;
 	}
 }
+static cs::extension array_iterator_ext;
+static cs::extension_t array_iterator_ext_shared=std::make_shared<cs::extension_holder>(&array_iterator_ext);
+namespace cs_any {
+	template<>cs::extension_t& get_ext<cs::array::iterator>()
+	{
+		return array_iterator_ext_shared;
+	}
+}
 namespace array_cs_ext {
 	using namespace cs;
+// Element access
+	cs::any at(const array& arr,number posit)
+	{
+		return arr.at(posit);
+	}
+	cs::any front(const array& arr)
+	{
+		return arr.front();
+	}
+	cs::any back(const array& arr)
+	{
+		return arr.back();
+	}
+// Iterators
+	array::iterator begin(array& arr)
+	{
+		return arr.begin();
+	}
+	array::iterator term(array& arr)
+	{
+		return arr.end();
+	}
+	array::iterator forward(array::iterator& it)
+	{
+		return ++it;
+	}
+	array::iterator backward(array::iterator& it)
+	{
+		return --it;
+	}
+	cs::any data(array::iterator& it)
+	{
+		return *it;
+	}
+// Capacity
+	bool empty(const array& arr)
+	{
+		return arr.empty();
+	}
+	number size(const array& arr)
+	{
+		return arr.size();
+	}
+// Modifiers
+	void clear(array& arr)
+	{
+		arr.clear();
+	}
+	array::iterator insert(array& arr,array::iterator& pos,const cs::any& val)
+	{
+		return arr.insert(pos,copy(val));
+	}
+	array::iterator erase(array& arr,array::iterator& pos)
+	{
+		return arr.erase(pos);
+	}
 	void push_front(array& arr,const cs::any& val)
 	{
 		arr.push_front(copy(val));
@@ -26,26 +90,24 @@ namespace array_cs_ext {
 	{
 		arr.pop_back();
 	}
-	void clear(array& arr)
-	{
-		arr.clear();
-	}
-	cs::any at(const array& arr,number posit)
-	{
-		return arr.at(posit);
-	}
-	number size(const array& arr)
-	{
-		return arr.size();
-	}
 	void init()
 	{
+		array_ext.add_var("at",cs::any::make_protect<native_interface>(cni(at),true));
+		array_ext.add_var("front",cs::any::make_protect<native_interface>(cni(front),true));
+		array_ext.add_var("back",cs::any::make_protect<native_interface>(cni(back),true));
+		array_ext.add_var("begin",cs::any::make_protect<native_interface>(cni(begin),true));
+		array_ext.add_var("term",cs::any::make_protect<native_interface>(cni(term),true));
+		array_iterator_ext.add_var("forward",cs::any::make_protect<native_interface>(cni(forward),true));
+		array_iterator_ext.add_var("backward",cs::any::make_protect<native_interface>(cni(backward),true));
+		array_iterator_ext.add_var("data",cs::any::make_protect<native_interface>(cni(data),true));
+		array_ext.add_var("empty",cs::any::make_protect<native_interface>(cni(empty),true));
+		array_ext.add_var("size",cs::any::make_protect<native_interface>(cni(size),true));
+		array_ext.add_var("clear",cs::any::make_protect<native_interface>(cni(clear),true));
+		array_ext.add_var("insert",cs::any::make_protect<native_interface>(cni(insert),true));
+		array_ext.add_var("erase",cs::any::make_protect<native_interface>(cni(erase),true));
 		array_ext.add_var("push_front",cs::any::make_protect<native_interface>(cni(push_front),true));
 		array_ext.add_var("pop_front",cs::any::make_protect<native_interface>(cni(pop_front),true));
 		array_ext.add_var("push_back",cs::any::make_protect<native_interface>(cni(push_back),true));
 		array_ext.add_var("pop_back",cs::any::make_protect<native_interface>(cni(pop_back),true));
-		array_ext.add_var("clear",cs::any::make_protect<native_interface>(cni(clear),true));
-		array_ext.add_var("at",cs::any::make_protect<native_interface>(cni(at),true));
-		array_ext.add_var("size",cs::any::make_protect<native_interface>(cni(size),true));
 	}
 }
--- tests/array.csc
+++ tests/array.csc
@@ -1,5 +1,30 @@
 var arr={}
+var it
+arr[6]=6
+arr[8]=8
 arr[10]=10
+it=arr.begin()
+while it!=arr.term()
+	if(it.data()==6)
+		it=arr.insert(it.forward(),7)
+	end
+	if(it.data()==8)
+		it=arr.erase(it)
+	end
+	it.forward()
+end
 for it iterate arr
-	system.println(it)
+	system.print(it," ")
+end
+system.println()
+it=arr.begin()
+while it!=arr.term()
+	system.print(it.data()," ")
+	it.forward()
+end
+system.println()
+it=(arr.term()).backward()
+while it!=(arr.begin()).backward()
+	system.print(it.data()," ")
+	it.backward()
 end
