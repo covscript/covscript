10296fd7d4cabe6b768525a0ee8b8c9e28852b89
Michael Lee <mikecovlee@163.com>
2017-08-06 12:16:16 +0800

    完善哈希表的功能

--- sources/extensions/hash_map_extension.hpp
+++ sources/extensions/hash_map_extension.hpp
@@ -10,6 +10,16 @@ namespace cs_any {
 }
 namespace hash_map_cs_ext {
 	using namespace cs;
+// Capacity
+	bool empty(const hash_map& map)
+	{
+		return map.empty();
+	}
+	number size(const hash_map& map)
+	{
+		return map.size();
+	}
+// Modifiers
 	void clear(hash_map& map)
 	{
 		map.clear();
@@ -25,25 +35,23 @@ namespace hash_map_cs_ext {
 	{
 		map.erase(key);
 	}
-	bool exist(hash_map& map,const cs::any& key)
-	{
-		return map.count(key)>0;
-	}
+// Lookup
 	cs::any at(hash_map& map,const cs::any& key)
 	{
 		return map.at(key);
 	}
-	number size(const hash_map& map)
+	bool exist(hash_map& map,const cs::any& key)
 	{
-		return map.size();
+		return map.count(key)>0;
 	}
 	void init()
 	{
+		hash_map_ext.add_var("empty",cs::any::make_protect<native_interface>(cni(empty),true));
+		hash_map_ext.add_var("size",cs::any::make_protect<native_interface>(cni(size),true));
 		hash_map_ext.add_var("clear",cs::any::make_protect<native_interface>(cni(clear),true));
 		hash_map_ext.add_var("insert",cs::any::make_protect<native_interface>(cni(insert),true));
 		hash_map_ext.add_var("erase",cs::any::make_protect<native_interface>(cni(erase),true));
-		hash_map_ext.add_var("exist",cs::any::make_protect<native_interface>(cni(exist),true));
 		hash_map_ext.add_var("at",cs::any::make_protect<native_interface>(cni(at),true));
-		hash_map_ext.add_var("size",cs::any::make_protect<native_interface>(cni(size),true));
+		hash_map_ext.add_var("exist",cs::any::make_protect<native_interface>(cni(exist),true));
 	}
 }
