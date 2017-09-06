696a7c49b0df78903a55b514253899f7d236a43c
Michael Lee <mikecovlee@163.com>
2017-09-06 08:14:35 +0800

    支持终端API

---
+++ sources/extensions/console/win32_conio.hpp
@@ -0,0 +1,109 @@
+#pragma once
+
+#include <windows.h>
+#include <cstring>
+#include <conio.h>
+#include <cstdlib>
+
+namespace cs_impl {
+	namespace conio {
+		static HANDLE StdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+		static int terminal_width()
+		{
+			static CONSOLE_SCREEN_BUFFER_INFO csbi;
+			GetConsoleScreenBufferInfo(StdHandle, &csbi);
+			return csbi.srWindow.Right - csbi.srWindow.Left;
+		}
+
+		static int terminal_height()
+		{
+			static CONSOLE_SCREEN_BUFFER_INFO csbi;
+			GetConsoleScreenBufferInfo(StdHandle, &csbi);
+			return csbi.srWindow.Bottom - csbi.srWindow.Top;
+		}
+
+		static void gotoxy(SHORT x, SHORT y)
+		{
+			SetConsoleCursorPosition(StdHandle, {x, y});
+		}
+
+		static void echo(bool mode)
+		{
+			static CONSOLE_CURSOR_INFO cci;
+			GetConsoleCursorInfo(StdHandle, &cci);
+			cci.bVisible = mode;
+			SetConsoleCursorInfo(StdHandle, &cci);
+		}
+		/*
+			class console final {
+				SHORT w, h;
+				std::size_t offset = 0;
+				CHAR_INFO *buffer = nullptr;
+				WORD attri = 0;
+			public:
+				console() = delete;
+
+				console(std::size_t width, std::size_t height) : w(width), h(height), buffer(new CHAR_INFO[w * h]) {}
+
+				~console()
+				{
+					delete[] buffer;
+				}
+
+				void set_color(int textcolor, int bgcolor)
+				{
+					attri = textcolor + (bgcolor - 8) * 16;
+				}
+
+				void reset()
+				{
+					set_color(15, 8);
+				}
+
+				void put_char(char ch)
+				{
+					buffer[offset].Attributes = attri;
+					buffer[offset].Char.AsciiChar = ch;
+					++offset;
+				}
+
+				void flush()
+				{
+					static CONSOLE_SCREEN_BUFFER_INFO csbi;
+					GetConsoleScreenBufferInfo(StdHandle, &csbi);
+					WriteConsoleOutput(StdHandle, buffer, {w, h}, {0, 0}, &csbi.srWindow);
+				}
+			};
+
+			static void set_title(const char *title)
+			{
+				SetConsoleTitle(title);
+			}
+
+			static void set_color(int textcolor, int bgcolor)
+			{
+				SetConsoleTextAttribute(StdHandle, textcolor + (bgcolor - 8) * 16);
+			}
+
+			static void reset()
+			{
+				set_color(15, 8);
+			}
+		*/
+		static void clrscr()
+		{
+			system("cls");
+		}
+
+		static int getch()
+		{
+			return ::getch();
+		}
+
+		static int kbhit()
+		{
+			return ::kbhit();
+		}
+	}
+}
--- sources/extensions/system_extension.hpp
+++ sources/extensions/system_extension.hpp
@@ -1,11 +1,65 @@
 #pragma once
 
+#include "./console/conio.hpp"
 #include "../cni.hpp"
 #include <iostream>
 #include <cstdlib>
 #include <limits>
 
 static cs::extension system_ext;
+static cs::extension console_ext;
+static cs::extension_t console_ext_shared = std::make_shared<cs::extension_holder>(&console_ext);
+namespace console_cs_ext {
+	using namespace cs;
+	using namespace cs_impl;
+
+	number terminal_width()
+	{
+		return conio::terminal_width();
+	}
+
+	number terminal_height()
+	{
+		return conio::terminal_height();
+	}
+
+	void gotoxy(number x,number y)
+	{
+		conio::gotoxy(x,y);
+	}
+
+	void echo(boolean v)
+	{
+		conio::echo(v);
+	}
+
+	void clrscr()
+	{
+		conio::clrscr();
+	}
+
+	char getch()
+	{
+		return conio::getch();
+	}
+
+	boolean kbhit()
+	{
+		return conio::kbhit();
+	}
+
+	void init()
+	{
+		console_ext.add_var("terminal_width", var::make_protect<callable>(cni(terminal_width)));
+		console_ext.add_var("terminal_height", var::make_protect<callable>(cni(terminal_height)));
+		console_ext.add_var("gotoxy", var::make_protect<callable>(cni(gotoxy)));
+		console_ext.add_var("echo", var::make_protect<callable>(cni(echo)));
+		console_ext.add_var("clrscr", var::make_protect<callable>(cni(clrscr)));
+		console_ext.add_var("getch", var::make_protect<callable>(cni(getch)));
+		console_ext.add_var("kbhit", var::make_protect<callable>(cni(kbhit)));
+	}
+}
+
 namespace system_cs_ext {
 	using namespace cs;
 
@@ -29,6 +83,8 @@ namespace system_cs_ext {
 
 	void init()
 	{
+		console_cs_ext::init();
+		system_ext.add_var("console", var::make_protect<extension_t>(console_ext_shared));
 		system_ext.add_var("max", var::make_constant<number>(std::numeric_limits<number>::max()));
 		system_ext.add_var("inf", var::make_constant<number>(std::numeric_limits<number>::infinity()));
 		system_ext.add_var("in", var::make_protect<istream>(&std::cin, [](std::istream *) {}));
