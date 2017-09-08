d13449af24dc5c8ec9b57f15610ffe81c58931fc
Michael Lee <mikecovlee@163.com>
2017-09-08 16:21:26 +0800

    修复错误

--- tests/extension.cpp
+++ tests/extension.cpp
@@ -5,12 +5,14 @@
 using namespace cs;
 static extension my_ext;
 
-number test(number a) {
-    std::cout << a << std::endl;
-    return a + 1;
+number test(number a)
+{
+	std::cout << a << std::endl;
+	return a + 1;
 }
 
-extension *cs_extension() {
-    my_ext.add_var("test", var::make_protect<callable>(cni(test), true));
-    return &my_ext;
+extension *cs_extension()
+{
+	my_ext.add_var("test", var::make_protect<callable>(cni(test), true));
+	return &my_ext;
 }
