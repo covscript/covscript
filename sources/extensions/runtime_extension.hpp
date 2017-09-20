c43bd857d9227b324d914aacc36354d81c8b37f4
Michael Lee <mikecovlee@163.com>
2017-09-20 13:09:19 +0800

    更换开源协议

--- sources/extensions/runtime_extension.hpp
+++ sources/extensions/runtime_extension.hpp
@@ -28,9 +28,21 @@ namespace runtime_cs_ext {
 
 	void info()
 	{
-		std::cout << "Covariant Script Programming Language Interpreter Version " << cs::version << "\n"
-		          "Copyright (C) 2017 Michael Lee.All rights reserved.\nThis program comes with ABSOLUTELY NO WARRANTY; for details see <http://www.gnu.org/licenses/>.\n"
-		          "This is free software, and you are welcome to redistribute it under certain conditions.\nPlease visit <http://covscript.org/> for more information." << std::endl;
+		std::cout << "Covariant Script Programming Language Interpreter\nVersion: " << cs::version << "\n"
+		          "Copyright (C) 2017 Michael Lee.All rights reserved.\n"
+		          "This program is free software: you can redistribute it and/or modify"
+		          "it under the terms of the GNU Affero General Public License as published"
+		          "by the Free Software Foundation, either version 3 of the License, or"
+		          "(at your option) any later version.\n"
+		          "\n"
+		          "This program is distributed in the hope that it will be useful,"
+		          "but WITHOUT ANY WARRANTY; without even the implied warranty of"
+		          "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
+		          "GNU Affero General Public License for more details.\n"
+		          "\n"
+		          "You should have received a copy of the GNU Affero General Public License"
+		          "along with this program.  If not, see <http://www.gnu.org/licenses/>.\n"
+		          "Please visit <http://covscript.org/> for more information." << std::endl;
 	}
 
 	number time()
