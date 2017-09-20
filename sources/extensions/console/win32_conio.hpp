a0b0e24656673ac2cc07089acd0aa29c6b6c56fa
Michael Lee <mikecovlee@163.com>
2017-09-20 12:58:19 +0800

    增加开源协议声明

--- sources/extensions/console/win32_conio.hpp
+++ sources/extensions/console/win32_conio.hpp
@@ -1,5 +1,24 @@
 #pragma once
-
+/*
+* Covariant Script Console
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Copyright (C) 2017 Michael Lee(李登淳)
+* Email: mikecovlee@163.com
+* Github: https://github.com/mikecovlee
+*/
 #include <windows.h>
 #include <cstring>
 #include <conio.h>
