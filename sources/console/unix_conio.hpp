7cf075d3eb930427431d6daaffb1734688c37283
Mike Lee <mikecovlee@163.com>
2017-10-05 19:10:02 +0800

    修复错误

--- sources/console/unix_conio.hpp
+++ sources/console/unix_conio.hpp
@@ -25,6 +25,24 @@
 #include <sys/ioctl.h>
 #include <sys/select.h>
 
+static void terminal_lnbuf(int yn) {
+   struct termios oldt, newt;
+   tcgetattr(0, &oldt);
+   newt = oldt;
+   if (!yn) newt.c_lflag &= ~ICANON;
+   else newt.c_lflag |= ICANON;
+   tcsetattr(0, TCSANOW, &newt);
+}
+ 
+static void terminal_echo(int yn) {
+   struct termios oldt, newt;
+   tcgetattr(0, &oldt);
+   newt = oldt;
+   if (!yn) newt.c_lflag &= ~ECHO;
+   else newt.c_lflag |= ECHO;
+   tcsetattr(0, TCSANOW, &newt);
+}
+
 namespace cs_impl {
 	namespace conio {
 		static int terminal_width()
@@ -48,28 +66,12 @@ namespace cs_impl {
 
 		static void echo(bool in)
 		{
-			struct termios oldt, newt;
-			tcgetattr(0, &oldt);
-			newt = oldt;
-			if (in) {
+			if (in)
 				printf("\33[?25h");
-				newt.c_lflag |= ECHO;
-				newt.c_lflag |= ICANON;
-			}
-			else {
+			else
 				printf("\33[?25l");
-				newt.c_lflag &= ~ECHO;
-				newt.c_lflag &= ~ICANON;
-			}
-			tcsetattr(0, TCSANOW, &newt);
 		}
 
-		/*
-		    static void reset()
-		    {
-		        printf("\e[37;40;0m");
-		    }
-		*/
 		static void clrscr()
 		{
 			printf("\x1B[2J\x1B[0;0f");
@@ -77,22 +79,28 @@ namespace cs_impl {
 
 		static int getch()
 		{
-			int ch;
-			ch = getchar();
+			terminal_lnbuf(0);
+			terminal_echo(0);
+			int ch = getchar();
+			terminal_lnbuf(1);
+			terminal_echo(1);
 			return ch;
 		}
 
 		static int kbhit()
 		{
-			int ret;
 			fd_set fds;
+			terminal_lnbuf(0);
+			terminal_echo(0);
 			struct timeval tv;
 			tv.tv_sec = 0;
 			tv.tv_usec = 0;
 			FD_ZERO(&fds);
 			FD_SET(0, &fds);
 			select(1, &fds, 0, 0, &tv);
-			ret = FD_ISSET(0, &fds);
+			int ret = FD_ISSET(0, &fds);
+			terminal_lnbuf(1);
+			terminal_echo(1);
 			return ret;
 		}
 	}
