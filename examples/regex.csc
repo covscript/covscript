d91879505c2da5d3354023ce8f5e400ce080dc50
Michael Lee <mikecovlee@163.com>
2017-11-03 17:31:38 +0800

    支持正则表达式

---
+++ examples/regex.csc
@@ -0,0 +1,15 @@
+import regex
+var reg=regex.build("(\\S+)")
+@begin
+var s="Some people, when confronted with a problem, think 
+\"I know, I'll use regular expressions.\"
+ Now they have two problems."
+@end
+loop
+    var res=reg.search(s)
+    if !res.ready()||res.empty()
+        break
+    end
+    system.out.println(res.str())
+    s=res.suffix()
+end
---
+++ sources/extensions/regex.cpp
@@ -0,0 +1,143 @@
+/*
+* Covariant Script Regex Extension
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Copyright (C) 2017 Michael Lee(李登淳)
+* Email: mikecovlee@163.com
+* Github: https://github.com/mikecovlee
+*/
+#include <regex>
+#include "../headers/extension.hpp"
+#include "../headers/cni.hpp"
+
+static cs::extension regex_ext;
+static cs::extension regex_result_ext;
+static cs::extension_t regex_ext_shared = std::make_shared<cs::extension_holder>(&regex_ext);
+static cs::extension_t regex_result_ext_shared = std::make_shared<cs::extension_holder>(&regex_result_ext);
+namespace cs_impl {
+	template<>
+	cs::extension_t &get_ext<std::regex>()
+	{
+		return regex_ext_shared;
+	}
+
+	template<>
+	cs::extension_t &get_ext<std::smatch>()
+	{
+		return regex_result_ext_shared;
+	}
+
+	template<>
+	constexpr const char *get_name_of_type<std::regex>()
+	{
+		return "cs::regex";
+	}
+
+	template<>
+	constexpr const char *get_name_of_type<std::smatch>()
+	{
+		return "cs::regex::result";
+	}
+}
+namespace regex_cs_ext {
+	using namespace cs;
+
+	var build(const string& str)
+	{
+		return var::make<std::regex>(str);
+	}
+
+	std::smatch match(std::regex& reg,const string& str)
+	{
+		std::smatch m;
+		std::regex_search(str,m,reg);
+		return std::move(m);
+	}
+
+	std::smatch search(std::regex& reg,const string& str)
+	{
+		std::smatch m;
+		std::regex_search(str,m,reg);
+		return std::move(m);
+	}
+
+	string replace(std::regex& reg,const string& str,const string& fmt)
+	{
+		return std::regex_replace(str,reg,fmt);
+	}
+
+	bool ready(const std::smatch& m)
+	{
+		return m.ready();
+	}
+
+	bool empty(const std::smatch& m)
+	{
+		return m.empty();
+	}
+
+	number length(const std::smatch& m)
+	{
+		return m.length();
+	}
+
+	number position(const std::smatch& m)
+	{
+		return m.position();
+	}
+
+	string str(const std::smatch& m)
+	{
+		return m.str();
+	}
+
+	string prefix(const std::smatch& m)
+	{
+		return m.prefix();
+	}
+
+	string suffix(const std::smatch& m)
+	{
+		return m.suffix();
+	}
+
+	string format(const std::smatch& m,const string& fmt)
+	{
+		return m.format(fmt);
+	}
+
+	void init()
+	{
+		regex_ext.add_var("result", var::make_protect<extension_t>(regex_result_ext_shared));
+		regex_ext.add_var("build", var::make_protect<callable>(cni(build)));
+		regex_ext.add_var("match", var::make_protect<callable>(cni(match)));
+		regex_ext.add_var("search", var::make_protect<callable>(cni(search)));
+		regex_ext.add_var("replace", var::make_protect<callable>(cni(replace)));
+		regex_result_ext.add_var("ready", var::make_protect<callable>(cni(ready)));
+		regex_result_ext.add_var("empty", var::make_protect<callable>(cni(empty)));
+		regex_result_ext.add_var("length", var::make_protect<callable>(cni(length)));
+		regex_result_ext.add_var("position", var::make_protect<callable>(cni(position)));
+		regex_result_ext.add_var("str", var::make_protect<callable>(cni(str)));
+		regex_result_ext.add_var("prefix", var::make_protect<callable>(cni(prefix)));
+		regex_result_ext.add_var("suffix", var::make_protect<callable>(cni(suffix)));
+		regex_result_ext.add_var("format", var::make_protect<callable>(cni(format)));
+	}
+}
+
+cs::extension *cs_extension()
+{
+	regex_cs_ext::init();
+	return &regex_ext;
+}
