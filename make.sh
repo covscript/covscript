e9181c3e66ba680b7e8323d0f5eda9f9c4db0df4
Michael Lee <mikecovlee@163.com>
2017-12-24 12:39:20 +0800

    更新构建脚本

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
-mkdir build
+mkdir -p build
 cd build
 mkdir bin
-g++ -std=c++11 -I ../include -fPIE -s -O3 ../sources/standalone.cpp -ldl -o ./bin/cs
-g++ -std=c++11 -I ../include -fPIE -s -O3 ../sources/repl.cpp -ldl -o ./bin/cs_repl
+g++ -std=c++11 -I ../include -fPIE -s -O3 ../sources/standalone.cpp -ldl -o ./bin/cs &
+g++ -std=c++11 -I ../include -fPIE -s -O3 ../sources/repl.cpp -ldl -o ./bin/cs_repl &
+wait
