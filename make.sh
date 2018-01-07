830f2ef987be213ec30966ed331f6fcaab2c284e
Michael Lee <mikecovlee@163.com>
2018-01-07 08:43:27 +0800

    支持指定Import目录

--- make.sh
+++ make.sh
@@ -1,6 +1,11 @@
+if [ "$#" != "1" ]; then
+    export PREFIX="/usr"
+else
+    export PREFIX=$1
+fi
 mkdir -p build
 cd build
 mkdir -p bin
-g++ -std=c++11 -I ../include -fPIE -s -O3 -DCS_IMPORT_PATH="/etc/covscript/imports" ../sources/standalone.cpp -ldl -o ./bin/cs &
-g++ -std=c++11 -I ../include -fPIE -s -O3 -DCS_IMPORT_PATH="/etc/covscript/imports" ../sources/repl.cpp -ldl -o ./bin/cs_repl &
+g++ -std=c++11 -I ../include -fPIE -s -O3 -DCS_IMPORT_PATH="$PREFIX/share/covscript/imports" ../sources/standalone.cpp -ldl -o ./bin/cs &
+g++ -std=c++11 -I ../include -fPIE -s -O3 -DCS_IMPORT_PATH="$PREFIX/share/covscript/imports" ../sources/repl.cpp -ldl -o ./bin/cs_repl &
 wait
