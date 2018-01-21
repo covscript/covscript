8b4f54424c98504288ae745df3142524f6f9b080
Michael Lee <mikecovlee@163.com>
2018-01-21 14:53:22 +0800

    完善路经查找

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -e
 SELF_DIR="$(dirname $(readlink -f $0))"
@@ -8,7 +8,6 @@ PREFIX="/usr"
 CXXFLAGS="-std=c++11 -I ../include -fPIE -s -O3 -ldl"
 LDFLAGS="-ldl"
 CXX=g++
-CP=cp
 
 while [[ "$1"x != ""x ]]; do
     arg="$1"; shift
@@ -21,12 +20,6 @@ done
 mkdir -p build
 cd build
 mkdir -p bin
-$CXX $CXXFLAGS -DCONFIG_CS_HOME="\"$PREFIX/share/covscript\"" \
-    ../sources/standalone.cpp $LDFLAGS -o ./bin/cs &
-$CXX $CXXFLAGS -DCONFIG_CS_HOME="\"$PREFIX/share/covscript\"" \
-    ../sources/repl.cpp $LDFLAGS -o ./bin/cs_repl &
+$CXX $CXXFLAGS -DCOVSCRIPT_HOME="\"$PREFIX/share/covscript\"" ../sources/standalone.cpp $LDFLAGS -o ./bin/cs &
+$CXX $CXXFLAGS -DCOVSCRIPT_HOME="\"$PREFIX/share/covscript\"" ../sources/repl.cpp $LDFLAGS -o ./bin/cs_repl &
 wait
-
-mkdir -p ./$PREFIX/{bin,share}
-mkdir -p ./$PREFIX/share/covscript/imports
-$CP ./bin/cs ./bin/cs_repl ./$PREFIX/bin
