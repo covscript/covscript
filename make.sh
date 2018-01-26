fa95763ea386b40efac84ce451d88a3d6af5f2c3
kiva <kiva515@foxmail.com>
2018-01-26 19:34:11 +0800

    Make: Support environment variables

--- make.sh
+++ make.sh
@@ -4,15 +4,39 @@ set -e
 SELF_DIR="$(dirname $(readlink -f $0))"
 cd "$SELF_DIR"
 
-PREFIX="/usr"
-CXXFLAGS="-std=c++11 -I ../include -fPIE -s -O3 -ldl"
-LDFLAGS="-ldl"
-CXX=g++
+function set_flag() {
+    local append=false
+
+    if [[ "$1" == "--append" ]]; then
+        shift
+        append=true
+    fi
+
+    local var="$1"; shift
+    declare -n ref="$var" 
+    if [[ "$ref" == "" ]]; then
+        ref="$@"
+    elif [[ "$append" == true ]]; then
+        ref="$ref $@"
+    fi
+}
+
+DEFAULT_PREFIX="/usr"
+DEFAULT_CXXFLAGS="-std=c++11 -I ../include -fPIE -s -O3 -ldl"
+DEFAULT_LDFLAGS="-ldl"
+DEFAULT_CXX=g++
+
+set_flag CXX $DEFAULT_CXX
+set_flag PREFIX $DEFAULT_PREFIX
+set_flag --append CXXFLAGS $DEFAULT_CXXFLAGS
+set_flag --append LDFLAGS $DEFAULT_LDFLAGS
 
 while [[ "$1"x != ""x ]]; do
     arg="$1"; shift
     case "$arg" in
         --prefix=* ) PREFIX="${arg##--prefix=}" ;;
+        --cxxflags=* ) CXXFLAGS="${arg##--cxxflags=}" ;;
+        --ldflags=* ) LDFLAGS="${arg##--ldflags=}" ;;
         "--" ) break ;;
     esac
 done
@@ -23,3 +47,4 @@ mkdir -p bin
 $CXX $CXXFLAGS -DCOVSCRIPT_HOME="\"$PREFIX/share/covscript\"" ../sources/standalone.cpp $LDFLAGS -o ./bin/cs &
 $CXX $CXXFLAGS -DCOVSCRIPT_HOME="\"$PREFIX/share/covscript\"" ../sources/repl.cpp $LDFLAGS -o ./bin/cs_repl &
 wait
+
