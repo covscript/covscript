633a7c49f537c0ab1140cfd9f637076bc909fff5
Michael Lee <mikecovlee@163.com>
2018-03-15 11:24:00 +0800

    更新资源文件和编译脚本

--- make.bat
+++ make.bat
@@ -2,6 +2,7 @@
 @cd build
 @mkdir bin
 @windres ..\cs.rc cs.o
+@windres ..\cs_repl.rc cs_repl.o
 @g++ -std=c++11 -I ..\include -static -fPIE -s -O3 ..\sources\standalone.cpp cs.o -o .\bin\cs.exe
-@g++ -std=c++11 -I ..\include -static -fPIE -s -O3 ..\sources\repl.cpp cs.o -o .\bin\cs_repl.exe
-@del /F /Q cs.o
+@g++ -std=c++11 -I ..\include -static -fPIE -s -O3 ..\sources\repl.cpp cs_repl.o -o .\bin\cs_repl.exe
+@del /F /Q *.o
