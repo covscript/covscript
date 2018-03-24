80976cba8d57e0e3761c090aeaf65138af2c7646
Michael Lee <mikecovlee@163.com>
2018-03-24 20:19:39 +0800

    更新Windows构建脚本

--- make.bat
+++ make.bat
@@ -2,9 +2,10 @@
 @cd build
 @mkdir bin
 @mkdir lib
+@windres -I ..\include ..\sources\covscript.rc dll.o
 @windres -I ..\include ..\sources\standalone.rc cs.o
 @windres -I ..\include ..\sources\repl.rc cs_repl.o
-@g++ -std=c++11 -I ..\include -shared -static -fPIC -s -O3 ..\sources\covscript.cpp -o .\lib\libcovscript.dll
+@g++ -std=c++11 -I ..\include -shared -static -fPIC -s -O3 ..\sources\covscript.cpp dll.o -o .\lib\libcovscript.dll
 @g++ -std=c++11 -I ..\include -static -fPIE -s -O3 ..\sources\standalone.cpp cs.o -o .\bin\cs.exe
 @g++ -std=c++11 -I ..\include -static -fPIE -s -O3 ..\sources\repl.cpp cs_repl.o -o .\bin\cs_repl.exe
 @del /F /Q *.o
