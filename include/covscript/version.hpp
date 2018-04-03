aacdb39bc8ffe9d153a22524ce5804cf48907811
Michael Lee <mikecovlee@163.com>
2018-04-03 11:54:40 +0800

    更新版本信息

--- include/covscript/version.hpp
+++ include/covscript/version.hpp
@@ -1,7 +1,37 @@
 #pragma once
+/*
+* Covariant Script Version Info
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Copyright (C) 2018 Michael Lee(李登淳)
+* Email: mikecovlee@163.com
+* Github: https://github.com/mikecovlee
+*
+* Version Format:
+*   1 . 0 . 0 . 0 (Preview/Unstable/Stable)
+*   |   |   |   |            |
+*   |   |   |   |          Status
+*   |   |   | Build
+*   |   | Minor
+*   | Major
+* Master
+*
+*/
 #define COVSCRIPT_VERSION_NUM 1,2,2,6
-#define COVSCRIPT_VERSION_STR "1.2.2(Beta6)"
-#define COVSCRIPT_STD_VERSION 20180302
+#define COVSCRIPT_VERSION_STR "1.2.2.6(Unstable)"
+#define COVSCRIPT_STD_VERSION 180401
 #if defined(__WIN32) || defined(WIN32)
 #define COVSCRIPT_PLATFORM_WIN32
 #endif
