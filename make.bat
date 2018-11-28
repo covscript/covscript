6a5f8d7f4603f429665e15c89a59bcfadec6b0fd
Michael Lee <mikecovlee@163.com>
2018-11-28 19:37:33 +0800

    更新构建脚本

--- make.bat
+++ make.bat
@@ -2,10 +2,11 @@
 @cd build
 @mkdir bin
 @mkdir lib
-@windres -I ..\include ..\sources\win32_rc\covscript.rc dll.o
 @windres -I ..\include ..\sources\win32_rc\standalone.rc cs.o
 @windres -I ..\include ..\sources\win32_rc\repl.rc cs_repl.o
-@g++ -std=c++11 -I ..\include -shared -static -fPIC -s -O3 ..\sources\covscript.cpp dll.o -o .\lib\libcovscript.dll
-@g++ -std=c++11 -I ..\include -static -fPIE -s -O3 ..\sources\standalone.cpp cs.o -o .\bin\cs.exe
-@g++ -std=c++11 -I ..\include -static -fPIE -s -O3 ..\sources\repl.cpp cs_repl.o -o .\bin\cs_repl.exe
+@set CXX_ARGS=-std=c++11 -I ..\include -static -fPIE -s -O3
+@g++ %CXX_ARGS% -c ..\sources\compiler\*.cpp ..\sources\instance\*.cpp ..\sources\*.cpp
+@ar -ro .\lib\libcovscript.a *.o
+@g++ %CXX_ARGS% standalone.o cs.o covscript.o -o .\bin\cs.exe -L.\lib -lcovscript
+@g++ %CXX_ARGS% repl.o cs_repl.o covscript.o -o .\bin\cs_repl.exe -L.\lib -lcovscript
 @del /F /Q *.o
