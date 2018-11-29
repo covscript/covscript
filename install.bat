6e165d9ce1d5e6ea72892ae3e24ad9cd40bed452
Michael Lee <mikecovlee@163.com>
2018-11-29 22:57:12 +0800

    更新构建脚本

--- install.bat
+++ install.bat
@@ -1,5 +1,7 @@
 @echo off
+mkdir build
 mkdir csdev
+xcopy /E /Y .\bin .\build\bin\
 xcopy /E /Y .\lib .\csdev\lib\
 xcopy /E /Y .\include .\csdev\include\
 @echo on
--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
+mkdir -p ./build/bin
 mkdir -p ./csdev/include
 mkdir -p ./csdev/lib
+cp -rf ./bin ./build
 cp -rf ./include ./csdev
 cp -rf ./lib ./csdev
