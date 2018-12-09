b3268f518a25908a3dfc49c343744b2102e4bf7a
Michael Lee <mikecovlee@163.com>
2018-12-09 17:34:39 +0800

    更新测试文件

---
+++ tests/function_invoker.cpp
@@ -0,0 +1,15 @@
+#include <covscript/covscript.hpp>
+
+int main(int argc, const char *argv[])
+{
+    if (argc <= 1)
+        return -1;
+    cs::context_t context = cs::create_context(cs::parse_cmd_args(argc - 1, argv + 1));
+    context->instance->compile(argv[1]);
+    context->instance->interpret();
+    cs::function_invoker<cs::number(cs::number, cs::number)> test(context, "test");
+    int result = test(1, 2);
+    cs::function_invoker<void(cs::number)> println(context, "system.out.println");
+    println(result);
+    return 0;
+}
