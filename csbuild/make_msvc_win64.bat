291ca243dfe758da8849f056984d2516c28fb031
Michael Lee <mikecovlee@163.com>
2019-05-04 19:24:24 +0800

    Update
    Fuck Windows!

--- csbuild/make_msvc_win64.bat
+++ csbuild/make_msvc_win64.bat
@@ -1,7 +1,7 @@
 @echo off
 set config=MinSizeRel
-mkdir cmake-build\msvc
-cd    cmake-build\msvc
+mkdir cmake-build\msvc_win64
+cd    cmake-build\msvc_win64
 cmake -G "Visual Studio 15 2017 Win64" ..\..
 cmake --build . --config %config% --target covscript
 cmake --build . --config %config% --target covscript_debug
@@ -12,8 +12,8 @@ cd ..\..
 rd /S /Q build
 mkdir build\bin
 mkdir build\lib
-xcopy /Y cmake-build\msvc\%config%\*.exe build\bin\
-xcopy /Y cmake-build\msvc\%config%\*.lib build\lib\
+xcopy /Y cmake-build\msvc_win64\%config%\*.exe build\bin\
+xcopy /Y cmake-build\msvc_win64\%config%\*.lib build\lib\
 rd /S /Q csdev
 mkdir csdev\include\covscript
 mkdir csdev\lib
