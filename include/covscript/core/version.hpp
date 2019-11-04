b575670979a4784229b7ce19ad39bf818de51e7c
Michael Lee <mikecovlee@163.com>
2019-11-04 18:26:16 +0800

    Version: 3.3.1 Build 9
    All tests passed in Windows, Linux and macOS

--- include/covscript/core/version.hpp
+++ include/covscript/core/version.hpp
@@ -27,8 +27,8 @@
 * Master
 *
 */
-#define COVSCRIPT_VERSION_NUM 3,3,1,8
-#define COVSCRIPT_VERSION_STR "3.3.1 Manis pentadactyla(Unstable) Build 8"
+#define COVSCRIPT_VERSION_NUM 3,3,1,9
+#define COVSCRIPT_VERSION_STR "3.3.1 Manis pentadactyla(Unstable) Build 9"
 #define COVSCRIPT_STD_VERSION 191101
-#define COVSCRIPT_API_VERSION 191103
+#define COVSCRIPT_API_VERSION 191104
 #define COVSCRIPT_ABI_VERSION 191103
