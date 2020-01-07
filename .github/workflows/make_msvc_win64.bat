e7eb6fe85341eff35e600da2b207cd37b5a3c824
Michael Lee <mikecovlee@163.com>
2020-01-07 16:13:16 +0800

    fix win64

---
+++ .github/workflows/make_msvc_win64.bat
@@ -0,0 +1,20 @@
+@echo off
+set config=MinSizeRel
+mkdir cmake-build\msvc_win64
+cd    cmake-build\msvc_win64
+cmake -G "Visual Studio 16 2019" -A x64 ..\..
+cmake --build . --config %config% --target covscript
+cmake --build . --config %config% --target covscript_debug
+cmake --build . --config %config% --target cs
+cmake --build . --config %config% --target cs_dbg
+cd ..\..
+rd /S /Q build
+mkdir build\bin
+mkdir build\lib
+xcopy /Y cmake-build\msvc_win64\%config%\*.exe build\bin\
+xcopy /Y cmake-build\msvc_win64\%config%\*.lib build\lib\
+rd /S /Q csdev
+mkdir csdev\include\covscript
+mkdir csdev\lib
+xcopy /E /Y include\covscript csdev\include\covscript
+xcopy /E /Y build\lib         csdev\lib\
