8b6b5450bf7e5ed367845a282997410b6b70264d
李登淳 <mikecovlee@163.com>
2022-04-06 14:57:01 +0800

    Hotfix: 3.3.5 Build 21
    + fix showing wrong line number when reporting error

--- include/covscript/core/version.hpp
+++ include/covscript/core/version.hpp
@@ -32,8 +32,8 @@
 * Master
 *
 */
-#define COVSCRIPT_VERSION_NUM 3,3,5,20
-#define COVSCRIPT_VERSION_STR "3.3.5 Manis pentadactyla(Stable) Build 20"
+#define COVSCRIPT_VERSION_NUM 3,3,5,21
+#define COVSCRIPT_VERSION_STR "3.3.5 Manis pentadactyla(Stable) Build 21"
 #define COVSCRIPT_STD_VERSION 210503
 #define COVSCRIPT_API_VERSION 210504
 #define COVSCRIPT_ABI_VERSION 210503
