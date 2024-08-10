f4a6149ce5e41b1863534819642387c1888103a6
Mike Lee <mikecovlee@163.com>
2024-08-10 23:17:41 +0800

    v3.4.3 Build 13

--- include/covscript/core/version.hpp
+++ include/covscript/core/version.hpp
@@ -37,8 +37,8 @@
 *   the last two digits are different, it will be regarded as a compatible
 *   version.
 */
-#define COVSCRIPT_VERSION_NUM 3,4,3,12
-#define COVSCRIPT_VERSION_STR "3.4.3 Castor fiber(Stable) Build 12"
+#define COVSCRIPT_VERSION_NUM 3,4,3,13
+#define COVSCRIPT_VERSION_STR "3.4.3 Castor fiber(Stable) Build 13"
 #define COVSCRIPT_STD_VERSION 210508
 #define COVSCRIPT_API_VERSION 230801
 #define COVSCRIPT_ABI_VERSION 230308
